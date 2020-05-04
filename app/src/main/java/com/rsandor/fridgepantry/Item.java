c1827c3dd1b86378675d21040dac0a4461938966
Raphael J. Sandor <rsandor@cs.odu.edu>
2020-05-03 20:56:53 -0400

    Created room entity
    Next step is to create the DAO.
    https://codelabs.developers.google.com/codelabs/android-room-with-a-view/#4
    
    I got this from the codelabs from Google themselves. I am then going to
    have the AddItemActivity use parcelable to pass an item object to the
    activity.

--- app/src/main/java/com/rsandor/fridgepantry/Item.java
+++ app/src/main/java/com/rsandor/fridgepantry/Item.java
@@ -1,15 +1,28 @@
 package com.rsandor.fridgepantry;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
+@Entity(tableName="item_table")
 public class Item {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "itemName")
     private String itemName;
+
     DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
     private Date date = new Date();
-    private int quanitiy;
+
+    private int quantity;
+    private String unit;
     private UUID id;
 
     public Item(){
@@ -40,11 +53,19 @@ public class Item {
         this.date = date;
     }
 
-    public int getQuanitiy() {
-        return quanitiy;
+    public int getQuantity() {
+        return quantity;
     }
 
-    public void setQuanitiy(int quanitiy) {
-        this.quanitiy = quanitiy;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
     }
 }
